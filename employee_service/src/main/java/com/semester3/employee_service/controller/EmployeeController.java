9e94d0f7853b91b69bc362ee36ebd9d7d65d8921
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2026-04-23 16:03:01 +0100

    Fixed nested repo issue

--- employee_service/src/main/java/com/semester3/employee_service/controller/EmployeeController.java
+++ employee_service/src/main/java/com/semester3/employee_service/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.semester3.employee_service.controller;
 import com.semester3.employee_service.dto.EmployeeDTO;
 import com.semester3.employee_service.service.EmployeeService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
